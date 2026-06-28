843986b0bea229ab5341f16b76f3ab0ac8c4327a
Lami97 <127411706+Lami97@users.noreply.github.com>
2026-06-28 17:45:07 +0200

    Add FAQCategory model classes

---
+++ ZenCare.Model/Requests/FAQCategoryInsertRequest.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ZenCare.Model.Requests
+{
+    public class FAQCategoryInsertRequest
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; } = true;
+    }
+}
---
+++ ZenCare.Model/SearchObjects/FAQCategorySearchObject.cs
@@ -0,0 +1,9 @@
+namespace ZenCare.Model.SearchObjects
+{
+    public class FAQCategorySearchObject : BaseSearchObject
+    {
+        public string? Name { get; set; }
+
+        public bool? IsActive { get; set; }
+    }
+}
