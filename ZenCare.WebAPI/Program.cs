f117ea553535c384fb22c6fe57cc976f91e7b761
Lami97 <127411706+Lami97@users.noreply.github.com>
2026-06-28 18:32:48 +0200

    Register FAQ module

--- ZenCare.WebAPI/Program.cs
+++ ZenCare.WebAPI/Program.cs
@@ -11,7 +11,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ZenCareDbContext>(options =>
     options.UseSqlServer(connectionString));
 
-builder.Services.AddAutoMapper(_ => { }, typeof(RoleProfile).Assembly, typeof(ServiceCategoryProfile).Assembly, typeof(ProductCategoryProfile).Assembly, typeof(ProductTypeProfile).Assembly, typeof(UnitOfMeasureProfile).Assembly, typeof(FAQCategoryProfile).Assembly);
+builder.Services.AddAutoMapper(_ => { }, typeof(RoleProfile).Assembly, typeof(ServiceCategoryProfile).Assembly, typeof(ProductCategoryProfile).Assembly, typeof(ProductTypeProfile).Assembly, typeof(UnitOfMeasureProfile).Assembly, typeof(FAQCategoryProfile).Assembly, typeof(FAQProfile).Assembly);
 
 builder.Services.AddScoped<IRoleService, RoleService>();
 builder.Services.AddScoped<IServiceCategoryService, ServiceCategoryService>();
@@ -19,6 +19,7 @@ builder.Services.AddScoped<IProductCategoryService, ProductCategoryService>();
 builder.Services.AddScoped<IProductTypeService, ProductTypeService>();
 builder.Services.AddScoped<IUnitOfMeasureService, UnitOfMeasureService>();
 builder.Services.AddScoped<IFAQCategoryService, FAQCategoryService>();
+builder.Services.AddScoped<IFAQService, FAQService>();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
