d01cf6ba6f01cff5826d31cb0e0496b5d62dbe4d
Lami97 <127411706+Lami97@users.noreply.github.com>
2026-06-28 15:13:47 +0200

    Add ServiceCategory model classes

---
+++ ZenCare.Model/SearchObjects/ServiceCategorySearchObject.cs
@@ -0,0 +1,9 @@
+namespace ZenCare.Model.SearchObjects
+{
+    public class ServiceCategorySearchObject : BaseSearchObject
+    {
+        public string? Name { get; set; }
+
+        public bool? IsActive { get; set; }
+    }
+}
