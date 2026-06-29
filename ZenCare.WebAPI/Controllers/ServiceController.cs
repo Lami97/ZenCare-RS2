889d276bc5b242931d91a34c89855c76d384bb2f
Lami97 <127411706+Lami97@users.noreply.github.com>
2026-06-29 19:39:02 +0200

    Add Service controller

---
+++ ZenCare.WebAPI/Controllers/ServiceController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using ZenCare.Model.Requests;
+using ZenCare.Model.Responses;
+using ZenCare.Model.SearchObjects;
+using ZenCare.Services.Interfaces;
+
+namespace ZenCare.WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ServiceController : ControllerBase
+{
+    private readonly IServiceService _serviceService;
+
+    public ServiceController(IServiceService serviceService)
+    {
+        _serviceService = serviceService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<PagedResult<ServiceResponse>>> GetAll([FromQuery] ServiceSearchObject? search)
+    {
+        var result = await _serviceService.GetAllAsync(search);
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ServiceResponse>> GetById(int id)
+    {
+        var result = await _serviceService.GetByIdAsync(id);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<ServiceResponse>> Create([FromBody] ServiceInsertRequest request)
+    {
+        var result = await _serviceService.InsertAsync(request);
+        return result;
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<ServiceResponse>> Update(int id, [FromBody] ServiceUpdateRequest request)
+    {
+        var result = await _serviceService.UpdateAsync(id, request);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _serviceService.DeleteAsync(id);
+        return NoContent();
+    }
+}
