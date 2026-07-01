9dd42784c4e7040720b26ff9595de9bf9737750d
Lami97 <127411706+Lami97@users.noreply.github.com>
2026-07-01 12:12:04 +0200

    Add Cart controller

---
+++ ZenCare.WebAPI/Controllers/CartController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using ZenCare.Model.Requests;
+using ZenCare.Model.Responses;
+using ZenCare.Model.SearchObjects;
+using ZenCare.Services.Interfaces;
+
+namespace ZenCare.WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CartController : ControllerBase
+{
+    private readonly ICartService _cartService;
+
+    public CartController(ICartService cartService)
+    {
+        _cartService = cartService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<PagedResult<CartResponse>>> GetAll([FromQuery] CartSearchObject? search)
+    {
+        var result = await _cartService.GetAllAsync(search);
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<CartResponse>> GetById(int id)
+    {
+        var result = await _cartService.GetByIdAsync(id);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<CartResponse>> Create([FromBody] CartInsertRequest request)
+    {
+        var result = await _cartService.InsertAsync(request);
+        return result;
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<CartResponse>> Update(int id, [FromBody] CartUpdateRequest request)
+    {
+        var result = await _cartService.UpdateAsync(id, request);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _cartService.DeleteAsync(id);
+        return NoContent();
+    }
+}
