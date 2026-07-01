c718ef6565f72cfac316bcc79461cb134accf88a
Lami97 <127411706+Lami97@users.noreply.github.com>
2026-07-01 10:22:06 +0200

    Add Purchase controller

---
+++ ZenCare.WebAPI/Controllers/PurchaseController.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Mvc;
+using ZenCare.Model.Requests;
+using ZenCare.Model.Responses;
+using ZenCare.Model.SearchObjects;
+using ZenCare.Services.Interfaces;
+
+namespace ZenCare.WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PurchaseController : ControllerBase
+{
+    private readonly IPurchaseService _purchaseService;
+
+    public PurchaseController(IPurchaseService purchaseService)
+    {
+        _purchaseService = purchaseService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<PagedResult<PurchaseResponse>>> GetAll([FromQuery] PurchaseSearchObject? search)
+    {
+        var result = await _purchaseService.GetAllAsync(search);
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<PurchaseResponse>> GetById(int id)
+    {
+        var result = await _purchaseService.GetByIdAsync(id);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<PurchaseResponse>> Create([FromBody] PurchaseInsertRequest request)
+    {
+        var result = await _purchaseService.InsertAsync(request);
+        return result;
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<PurchaseResponse>> Update(int id, [FromBody] PurchaseUpdateRequest request)
+    {
+        var result = await _purchaseService.UpdateAsync(id, request);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _purchaseService.DeleteAsync(id);
+        return NoContent();
+    }
+}
